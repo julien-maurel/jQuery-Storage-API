4b6cf54635ac2f0f9e981ce64bb966f83680d206
Julien Maurel <julien@photobookshop.com.au>
2013-08-05 14:57:48 +1000

    Add some comments Bug fix on "set" function if 3 arguments and last is an array Add possibility to have an array on last argument on infinite arguments mode

--- jquery.storageapi.js
+++ jquery.storageapi.js
@@ -9,7 +9,7 @@
  * Project home:
  * https://github.com/julien-maurel/jQuery-Storage-API
  *
- * Version: 1.4.0
+ * Version: 1.5.0
  *
  */
 (function($){
@@ -17,12 +17,12 @@
   var cookie_local_prefix="ls_";
   var cookie_session_prefix="ss_";
 
-  // Get a variable in a storage. Infinite arguments to be able to get property in objects
-  // If second argument is an array, return an object with each property
+  // Get items from a storage
   function _get(storage){
-    var l=arguments.length,s=window[storage],a=arguments,a1=a[1],vi,ret;
-    if(l<2) throw new Error('Minimum 2 parameters must be given');
+    var l=arguments.length,s=window[storage],a=arguments,a1=a[1],vi,ret,tmp;
+    if(l<2) throw new Error('Minimum 2 arguments must be given');
     else if($.isArray(a1)){
+      // If second argument is an array, return an object with value of storage for each item in this array
       ret={};
       for(var i in a1){
         vi=a1[i];
@@ -34,52 +34,69 @@
       }
       return ret;
     }else if(l==2){
+      // If only 2 arguments, return value directly
       try{
         return JSON.parse(s.getItem(a1));
       }catch(e){
         return s.getItem(a1);
       }
     }else{
+      // If more than 2 arguments, parse storage to retrieve final value to return it
+      // Get first level
       try{
         ret=JSON.parse(s.getItem(a1));
       }catch(e){
         throw new ReferenceError(a1+' is not defined in this storage');
       }
+      // Parse next levels
       for(var i=2;i<l-1;i++){
         ret=ret[a[i]];
         if(ret===undefined) throw new ReferenceError([].slice.call(a,1,i+1).join('.')+' is not defined in this storage');
       }
-      return ret[a[i]];
+      // If last argument is an array, return an object with value for each item in this array
+      // Else return value
+      if($.isArray(a[i])){
+        tmp=ret;
+        ret={};
+        for(var j in a[i]){
+          ret[a[i][j]]=tmp[a[i][j]];
+        }
+        return ret;
+      }else{
+        return ret[a[i]];
+      }
     }
   }
 
-  // Set a variable in a storage. Infinite arguments to be able to set property in objects.
-  // If second argument is an object, each property is set
+  // Set items of a storage
   function _set(storage){
     var l=arguments.length,s=window[storage],a=arguments,a1=a[1],a2=a[2],vi,to_store={},tmp;
-    if(l<2 || !$.isPlainObject(a1) && l<3) throw new Error('Minimum 3 parameters must be given or second parameter must be an object');
+    if(l<2 || !$.isPlainObject(a1) && l<3) throw new Error('Minimum 3 arguments must be given or second parameter must be an object');
     else if($.isPlainObject(a1)){
+      // If first argument is an object, set values of storage for each property of this object
       for(var i in a1){
         vi=a1[i];
         if(!$.isPlainObject(vi)) s.setItem(i,vi);
         else s.setItem(i,JSON.stringify(vi));
       }
       return a1;
-    }else if(l==3 && !$.isPlainObject(a2)){
-      s.setItem(a1,a2);
-      return a2;
     }else if(l==3){
-      s.setItem(a1,JSON.stringify(a2));
+      // If only 3 arguments, set value of storage directly
+      if(typeof a2==='object') s.setItem(a1,JSON.stringify(a2));
+      else s.setItem(a1,a2);
       return a2;
     }else{
+      // If more than 3 arguments, parse storage to retrieve final node and set value
+      // Get first level
       try{
-        tmp = s.getItem(a1);
-        if(tmp != null) {
+        tmp=s.getItem(a1);
+        if(tmp!=null) {
           to_store=JSON.parse(tmp);
         }
       }catch(e){
       }
       tmp=to_store;
+      // Parse next levels and set value
       for(var i=2;i<l-2;i++){
         vi=a[i];
         if(!tmp[vi] || !$.isPlainObject(tmp[vi])) tmp[vi]={};
@@ -91,25 +108,29 @@
     }
   }
 
-  // Delete a variable in a storage. Infinite arguments to be able to delete property in objects
-  // If second argument is an array, each property is deleted
+  // Remove items from a storage
   function _remove(storage){
     var l=arguments.length,s=window[storage],a=arguments,a1=a[1],to_store,tmp;
-    if(l<2) throw new Error('Minimum 2 parameters must be given');
+    if(l<2) throw new Error('Minimum 2 arguments must be given');
     else if($.isArray(a1)){
+      // If first argument is an array, remove values from storage for each item of this array
       for(var i in a1){
         s.removeItem(a1[i]);
       }
       return true;
     }else if(l==2){
+      // If only 2 arguments, remove value from storage directly
       s.removeItem(a1);
       return true;
     }else{
+      // If more than 2 arguments, parse storage to retrieve final node and remove value
+      // Get first level
       try{
         to_store=tmp=JSON.parse(s.getItem(a1));
       }catch(e){
         throw new ReferenceError(a1+' is not defined in this storage');
       }
+      // Parse next levels and remove value
       for(var i=2;i<l-1;i++){
         tmp=tmp[a[i]];
         if(tmp===undefined) throw new ReferenceError([].slice.call(a,1,i).join('.')+' is not defined in this storage');
@@ -120,12 +141,13 @@
     }
   }
 
-  // Delete all variables in a storage
+  // Remove all items from a storage
   function _removeAll(storage, reinit_ns){
     var keys=_keys(storage);
     for(var i in keys){
       _remove(storage,keys[i]);
     }
+    // Reinitialize all namespace storages
     if(reinit_ns){
       for(var i in $.namespaceStorages){
         _createNamespace(i);
@@ -133,17 +155,20 @@
     }
   }
 
-  // Check if a variable is empty in a storage
+  // Check if items of a storage are empty
   function _isEmpty(storage){
     var l=arguments.length,a=arguments,s=window[storage],a1=a[1];
     if(l==1){
+      // If only one argument, test if storage is empty
       return (_keys(storage).length==0);
     }else if($.isArray(a1)){
+      // If first argument is an array, test each item of this array and return true only if all items are empty
       for(var i=0; i<a1.length;i++){
         if(!_isEmpty(storage,a1[i])) return false;
       }
       return true;
     }else{
+      // If more than 1 argument, try to get value and test it
       try{
         var v=_get.apply(this, arguments);
         return(
@@ -157,16 +182,18 @@
     }
   }
 
-  // Check if a variable exists in a storage
+  // Check if items of a storage exist
   function _isSet(storage){
     var l=arguments.length,a=arguments,s=window[storage],a1=a[1];
-    if(l<2) throw new Error('Minimum 2 parameters must be given');
+    if(l<2) throw new Error('Minimum 2 arguments must be given');
     if($.isArray(a1)){
+      // If first argument is an array, test each item of this array and return true only if all items exist
       for(var i=0; i<a1.length;i++){
         if(!_isSet(storage,a1[i])) return false;
       }
       return true;
     }else{
+      // For other case, try to get value and test it
       try{
         var v=_get.apply(this, arguments);
         return (v!==undefined && v!==null);
@@ -176,15 +203,18 @@
     }
   }
 
-  // Get keys of an objet in a storage or keys of a storage
+  // Get keys of a storage or of an item of the storage
   function _keys(storage){
     var l=arguments.length,s=window[storage],a=arguments,a1=a[1],keys=[],o={};
+    // If more than 1 argument, get value from storage to retrieve keys
+    // Else, use storage to retrieve keys
     if(l>1){
       o=_get.apply(this,a);
     }else{
       o=s;
     }
     if(o._cookie){
+      // If storage is a cookie, use $.cookie to retrieve keys
       for(var key in $.cookie()){
         if(key!='') {
           keys.push(key.replace(o._prefix,''));
@@ -222,32 +252,22 @@
     // Get a variable. If no parameters and storage have a namespace, return all namespace
     get:function(){
       var l=arguments.length,a=arguments,a0=a[0],vi;
-      if(!$.isArray(a0)){
-        var p = [this._type];
-        if(this._ns) p.push(this._ns);
-        [].unshift.apply(a,p);
-        return _get.apply(this, a);
-      }else if(this._ns){
-        var ret={};
-        for(var i in a0){
-          vi=a0[i];
-          ret[vi]=_get(this._type,this._ns,vi);
-        }
-        return ret;
-      }else{
-        return _get(this._type,a0);
-      }
+      if(l<1) throw new Error('Minimum 1 argument must be given or first parameter must be an object');
+      var p = [this._type];
+      if(this._ns) p.push(this._ns);
+      [].unshift.apply(a,p);
+      return _get.apply(this, a); 
     },
     // Set a variable
     set:function(){
       var l=arguments.length,a=arguments,a0=a[0];
-      if(l<1 || !$.isPlainObject(a0) && l<2) throw new Error('Minimum 2 parameters must be given or first parameter must be an object');
+      if(l<1 || !$.isPlainObject(a0) && l<2) throw new Error('Minimum 2 arguments must be given or first parameter must be an object');
       if(!$.isPlainObject(a0)){
         var p=[this._type];
         if(this._ns) p.push(this._ns);
         [].unshift.apply(a,p);
-	var r=_set.apply(this,a);
-	if(this._ns) return r[a0];
+        var r=_set.apply(this,a);
+        if(this._ns) return r[a0];
         else return r;
       }else if(this._ns){
         for(var i in a0){
@@ -261,7 +281,7 @@
     // Delete a variable.
     remove:function(){
       var l=arguments.length,a=arguments,a0=a[0];
-      if(l<1) throw new Error('Minimum 1 parameter must be given');
+      if(l<1) throw new Error('Minimum 1 argument must be given');
       if(!$.isArray(a0)){
         var p=[this._type];
         if(this._ns) p.push(this._ns);
@@ -305,7 +325,7 @@
     // Variable exists
     isSet:function(){
       var l=arguments.length,a=arguments,a0=a[0];
-      if(l<1) throw new Error('Minimum 1 parameter must be given');
+      if(l<1) throw new Error('Minimum 1 argument must be given');
       if(!$.isArray(a0)){
         var p=[this._type];
         if(this._ns) p.push(this._ns);
